aabc2b967491959e75d48b91d0c77562432bd216
arthurafonso <afaa@cesar.school>
2026-07-14 18:44:42 -0300

    Update config_app.py

--- config_app.py
+++ config_app.py
@@ -3,8 +3,8 @@
 # =====================================================================
 
 # Substitua com as credenciais que você copiou do seu painel do Supabase
-SUPABASE_URL = "https://mglxjfnzasyghrqgcvog.supabase.co/" 
-SUPABASE_KEY = "sb_publishable_UQ_AIe0hBIkzthuCWcpegw_Xt3-VUxt"
+SUPABASE_URL = "" 
+SUPABASE_KEY = ""
 
 # ID Único deste consultório para evitar mistura de dados entre clínicas
 CONSULTORIO_ID = "consultorio_clinica_a"
